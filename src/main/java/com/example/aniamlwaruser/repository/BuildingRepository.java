a565bc750fb638727759154791cb2513ebb2eaee
ajtwu1235 <ajtwu1235@gmail.com>
2023-10-20 14:42:42 +0900

    adadfad

---
+++ src/main/java/com/example/aniamlwaruser/repository/BuildingRepository.java
@@ -0,0 +1,7 @@
+package com.example.aniamlwaruser.repository;
+
+import com.example.aniamlwaruser.domain.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building,Long> {
+}
