562076529abb66f00868bd4a1f621bd201637d09
sein <lsi6601@gmail.com>
2023-10-23 22:24:27 +0900

    mix data build data add

---
+++ src/main/java/com/example/aniamlwaruser/controller/AnimalController.java
@@ -0,0 +1,4 @@
+package com.example.aniamlwaruser.controller;
+
+public class AnimalController {
+}
---
+++ src/main/java/com/example/aniamlwaruser/repository/BuildingRepository.java
@@ -0,0 +1,10 @@
+package com.example.aniamlwaruser.repository;
+
+import com.example.aniamlwaruser.domain.entity.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BuildingRepository extends JpaRepository<Building, Long> {
+}
