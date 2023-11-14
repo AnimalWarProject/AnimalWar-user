3112d2c45b52f44b532364ab2ce9856dfeccdc2d
sein <lsi6601@gmail.com>
2023-11-14 15:44:20 +0900

    merge

--- src/main/java/com/example/aniamlwaruser/repository/BuildingINVTRepository.java
+++ src/main/java/com/example/aniamlwaruser/repository/BuildingINVTRepository.java
@@ -1,5 +1,6 @@
 package com.example.aniamlwaruser.repository;
 
+import com.example.aniamlwaruser.domain.entity.Grade;
 import com.example.aniamlwaruser.domain.entity.UserBuilding;
 import com.example.aniamlwaruser.domain.response.GetAllResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,6 @@ public interface BuildingINVTRepository extends JpaRepository<UserBuilding,Long>
     @Query("SELECT b FROM UserBuilding b WHERE b.user.userUUID=:userUUID")
     List<GetAllResponse> findByUserUUID(UUID userUUID);
 
+    @Query("SELECT ub FROM UserBuilding ub WHERE ub.user.userUUID=:userUUID AND ub.building.grade=:grade")
+    List<UserBuilding> findAllByGrade(UUID userUUID, Grade grade);
 }
